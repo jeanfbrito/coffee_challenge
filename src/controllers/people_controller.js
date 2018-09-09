6bd837b41545c424e4f74d3122a1ccd7a6f99360
Jean Brito <jeanfbrito@gmail.com>
2018-09-09 19:41:54 -0300

    solve bug on create new person

--- src/controllers/people_controller.js
+++ src/controllers/people_controller.js
@@ -18,7 +18,7 @@ app.controller('PeopleController', function($localStorage,$scope) {
         if (peopleList.people[i].name === peopleList.personName) contains = true;
       }
       if(!contains) peopleList.people.push({name:peopleList.personName});
-      peopleList.people = peopleList.$storage.peopleList;
+      peopleList.$storage.peopleList = peopleList.people;
     }
     peopleList.personName = '';
   };
