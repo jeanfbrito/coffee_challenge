ac132bc5b363148d79af1e2d42fc4e79e3429c8b
Jean Brito <jeanfbrito@gmail.com>
2018-09-09 03:06:22 -0300

    add code comments

--- src/controllers/people_controller.js
+++ src/controllers/people_controller.js
@@ -2,11 +2,15 @@ app.controller('PeopleController', function($localStorage,$scope) {
   var peopleList = this;
   $scope.weekday = [];
   this.$storage = $localStorage;
+
+  //if is the first time running this app, it will add placeholder names
   peopleList.people = peopleList.$storage.peopleList || [
     {name:'Joao'},
     {name:'Vitor'},
     {name:'Jean'},
     {name:'Carolina'}];
+
+  //add a person if it is not saved already  
   peopleList.addPerson = function() {
     if(peopleList.personName.length > 2){
       contains = false;
@@ -16,23 +20,30 @@ app.controller('PeopleController', function($localStorage,$scope) {
       if(!contains) peopleList.people.push({name:peopleList.personName});
     }
     peopleList.personName = '';
-    peopleList.sortWeek();
   };
+
+  //delete person using the $index
   peopleList.deletePerson = function(id) {
     peopleList.people.splice(id, 1);
   };
+
+  //use the algorithm to shuffle the names in the storage and add a name to each weekday and shift
   peopleList.sortWeek = function(){
     $scope.weekday  = [];
+    //copy the variable to dont get the base storage shuffled
     var shuffledPeople = angular.copy(peopleList.people);
     shuffledPeople = shuffleArray(shuffledPeople);
+    //distribute the names on the 10 shifts
     var i,s = shuffledPeople.length;
     for (i = 0; i < 10; i++) {
       if(s == 0 ) s = shuffledPeople.length;
       s--;
       $scope.weekday.push(shuffledPeople[s])
     }
-    console.log($scope.weekday );
   };
+
+  //shuffle an array using Fisher-Yates algorithm
+  //https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
   var shuffleArray = function shuffle(arr) {
     var i,j,temp;
     for (i = arr.length - 1; i > 0; i--) {
